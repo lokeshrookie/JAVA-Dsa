4249e453c667d184822989e5c006746160b21168
Lokesh <rocklinglokesh@gmail.com>
2021-09-25 10:46:29 +0530

    StringAscIISum added

---
+++ src/com/Lokesh/Strings/StringBuilderReverse.java
@@ -0,0 +1,22 @@
+package com.Lokesh.Strings;
+public class StringBuilderReverse {
+    public static void main(String[] args) {
+        String str = "Hello World! How are you";
+        //---- method 1 .
+//        String out = reverseString(str);
+//        System.out.print(out);
+        //----method 2.
+//        System.out.println(reverseString(str));
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+        String strings = "Hello World! How are you";
+//        System.out.println(sb.compareTo(strings));
+//        System.out.println();
+    }
+//    static String reverseString(String s ){
+//        StringBuilder sb = new StringBuilder(s);
+//        sb.reverse();
+//        return sb.toString();
+//    }
+
+}
