900d6d2845fea02ff28a068f68576fd158f93593
Lokesh <rocklinglokesh@gmail.com>
2021-10-03 18:26:49 +0530

    initail commit

---
+++ src/com/Lokesh/Search/LinearJet.java
@@ -0,0 +1,28 @@
+package com.Lokesh.Search;
+
+import java.util.Scanner;
+
+public class LinearJet {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter array size : ");
+        int size = s.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements");
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println("Enter an element to search");
+        int target = s.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+//            if(arr.length == 0){
+//                return -1;
+//            }
+
+            if(arr[i] == target){
+                System.out.println("Target is found at index: " + i);
+                break;
+            }
+        }
+    }
+}
---
+++ src/com/Lokesh/Sorting/BubbleSortJet.java
@@ -0,0 +1,24 @@
+package com.Lokesh.Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSortJet {
+    public static void main(String[] args) {
+        int[] arr2 = {3, 46, -5, 9};
+        int[] arr3 = {'c'};
+//        System.out.println(Arrays.toString(new int[]{'c','d','e'}));
+        System.out.println(Arrays.toString(bubbleSort(arr2)));
+    }
+    static int[] bubbleSort(int[] arr){
+        int min = arr[0];
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < min){
+                temp = min;
+                min = arr[i];
+                arr[i] = temp;
+            }
+        }
+        return arr;
+    }
+}
