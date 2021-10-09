1fbf3acb86146b1caeacaeefbdf9f3e3206eabf0
Lokesh <rocklinglokesh@gmail.com>
2021-10-09 23:17:33 +0530

    initial commit

--- src/com/Lokesh/Arrays/ArrayDeletion.java
+++ src/com/Lokesh/Arrays/ArrayDeletion.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ArrayDeletion
 {
 	public static void main(String[] args) {
-// 		System.out.println("Hello World");
+////System.out.println("Hello World");
 int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 int index = 3;
 System.out.println(remove(array, index));
