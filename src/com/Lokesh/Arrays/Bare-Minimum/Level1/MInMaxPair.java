85abe3d9c4939d20a2d91aca68f46786a8759133
Lokesh <rocklinglokesh@gmail.com>
2021-10-09 23:15:40 +0530

    initial commit

---
+++ src/com/Lokesh/Arrays/Bare-Minimum/Level1/MInMaxPair.java
@@ -0,0 +1,38 @@
+import org.graalvm.compiler.lir.ssa.SSAUtil.PhiValueVisitor;
+
+public class MInMaxPair 
+{
+
+    Public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int size  = arr.length;
+        System.out.println(getMinMax(arr, size));
+
+    }
+    static pair getMinMax(long arr[], long n)  
+    {
+        //Write your code here
+         long min = arr[0], max = arr[0];
+    // loop for finding min 
+    for(int i = 0; i<n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+        else{
+            continue;
+        }
+    }
+    //loop for finding max
+    for(int i = 0; i<n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        else{
+            continue;
+        }
+    }
+    // String output = 
+    return new pair(min, max);
+    }
+}
+
