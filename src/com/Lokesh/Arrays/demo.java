dc3b23abf73e29fb783610e4f23efebd11c58c81
Lokesh <rocklinglokesh@gmail.com>
2021-10-10 23:17:14 +0530

    initial commit

--- src/com/Lokesh/Arrays/demo.java
+++ src/com/Lokesh/Arrays/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         int[] arr = {5, 5, 6, 6, 7, 7,};
            System.out.println("Elements of the array are: " + Arrays.toString(arr));
+           System.out.println("Hello");
     }
 }
