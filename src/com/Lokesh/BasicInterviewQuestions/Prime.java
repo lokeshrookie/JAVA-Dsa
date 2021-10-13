615fac20d5f2e56f737812cd4d920269a0d087a8
Lokesh <rocklinglokesh@gmail.com>
2021-10-13 23:03:12 +0530

    initial commit

--- src/com/Lokesh/BasicInterviewQuestions/Prime.java
+++ src/com/Lokesh/BasicInterviewQuestions/Prime.java
@@ -1,7 +1,6 @@
 package com.Lokesh.BasicInterviewQuestions;
 
 import java.util.Scanner;
-
 public class Prime {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
