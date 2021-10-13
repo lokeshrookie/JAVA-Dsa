757e4070a7522136f6c92766ccca017362123839
Lokesh <rocklinglokesh@gmail.com>
2021-10-13 23:21:54 +0530

    initial commit

--- src/com/Lokesh/BasicInterviewQuestions/LcmHcfOptimised.java
+++ src/com/Lokesh/BasicInterviewQuestions/LcmHcfOptimised.java
@@ -1,4 +1,19 @@
 package com.Lokesh.BasicInterviewQuestions;
 
-public class LcmHcfOptimised {
+public class LcmHcfOptimised
+{
+    public static void main(String[] args) {
+        System.out.println(gcd(4, 9));
+//        System.out.println(lcm(2, 7));
+    }
+    static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
 }
+
