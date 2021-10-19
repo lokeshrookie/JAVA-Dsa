c32e9abce1bbcf5cdfe0f4c52365922a94c9aa4e
Lokesh <rocklinglokesh@gmail.com>
2021-10-19 13:48:28 +0530

    InsertionSort.java added

--- src/com/Lokesh/Sorting/InsertionSort.java
+++ src/com/Lokesh/Sorting/InsertionSort.java
@@ -16,16 +16,17 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {534,-23,0,232,4,11,-1213,3};
+//        int[] arr = {534,-23,0,232,4,11,-1213,3};
+        int[] arr = {5,4,3,2,1};
+        System.out.println("Before Sorting: " + Arrays.toString(arr) );
         insertion(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println( "\n After Sorting: " +Arrays.toString(arr));
     }
 
     static int[] insertion(int[] arr){
-
         for (int i = 0; i <arr.length-1; i++) { // i runs till n-1 . if i = 2, j runs 3 times, j=(3,2,1) // if i = 3, j = (4,3,2,1).
             for (int j = i+1 ; j > 0 ; j--) {
-                if(arr[j] < arr[j-1]){    // if false, break.
+                if(arr[j] < arr[j-1]){
                     swap(arr, j, j-1);
                 }
                 else
@@ -33,9 +34,16 @@ public class InsertionSort {
                     break; // because left hand side elements are sorted.
                 }
             }
+            /*
+               use the two lines to print sorted array after step.
+               System.out.println();
+               System.out.println(Arrays.toString(arr));
+
+             */
         }
         return arr;
     }
+
 
     static void swap(int[] arr, int first, int second){
         int temp = arr[first];
@@ -51,5 +59,7 @@ public class InsertionSort {
 5 4 3 2 1
  5 4 3 2 1
 
- i = 0:     4 5 3 2 1
+ i = 0:      4 5 3 2 1, break
+ i = 1:      4 3 5 2 1, 4 3 5 2 1, break
+ i = 2:      4 3 2 5 1, 4 2 3 5 1 ,
  */
