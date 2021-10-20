b96f93b1c7ff4d64a915fae28742badd62ebc411
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 16:14:39 +0530

    initial commit

--- src/com/Lokesh/BasicInterviewQuestions/Armstrong.java
+++ src/com/Lokesh/BasicInterviewQuestions/Armstrong.java
@@ -6,9 +6,13 @@ public class Armstrong {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int number = s.nextInt();
-        System.out.println(isArmstrong(number));
+//        System.out.println(isArmstrong(number));
+        for (int i = 1; i <=number; i++) {
+            if(armstrong2(i)){
+                System.out.println(i);
+            }
+        }
         s.close();
-
     }
     static boolean isArmstrong(int n) {
         int original = n;
@@ -20,5 +24,50 @@ public class Armstrong {
         }
         return sum == original;
     }
+
+    static  boolean armstrong2(int n ){
+        int org  = n;
+        int sum = 0;
+        while (n>0){
+            int rem = n%10;
+            n = n/10;
+            sum = sum + rem * rem * rem;
+        }
+        return sum == org;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }
--- src/com/Lokesh/BasicInterviewQuestions/LcmHcfOptimised.java
+++ src/com/Lokesh/BasicInterviewQuestions/LcmHcfOptimised.java
@@ -4,7 +4,7 @@ public class LcmHcfOptimised
 {
     public static void main(String[] args) {
         System.out.println(gcd(5, 1));
-        System.out.println(lcm(2, 7));
+//        System.out.println(lcm(2, 7));
     }
     // gcd
     static int gcd(int a, int b) {
@@ -14,8 +14,8 @@ public class LcmHcfOptimised
         return gcd(b%a, a);
     }
     //lcm using gcd
-    static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
-    }
+//    static int lcm(int a, int b) {
+//        return a * b / gcd(a, b);
+//    }
 }
 
