65a56c99c6d9bb4436d99f3479c7e6cf1eeaebb0
Lokesh <rocklinglokesh@gmail.com>
2021-10-22 23:37:33 +0530

    initial commit

--- src/com/Lokesh/Arrays/ArrayDeletion.java
+++ src/com/Lokesh/Arrays/ArrayDeletion.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class ArrayDeletion
 {
 	public static void main(String[] args) {
-////System.out.println("Hello World");
+
 int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 int index = 3;
 System.out.println(remove(array, index));
 	}
 	static String remove(int arr[], int index){
-	    for(int i = index; i<arr.length-1; i++){
+	    for(int i = index; i<=arr.length-1; i++){
+	    	if(i == arr.length - 1){
+	    		continue;
+			}
 	        arr[i] = arr[i+1];
 	    }
 	    return Arrays.toString(arr);
--- src/com/Lokesh/HashMap.java
+++ src/com/Lokesh/HashMap.java
@@ -1,7 +1,7 @@
 package com.Lokesh;
 public class HashMap {
     public static void main(String[] args) {
-        HashMap<Integer,Integer > hm = new HashMap();
+//        HashMap<Integer,Integer > hm = new HashMap();
 
     }
 }
