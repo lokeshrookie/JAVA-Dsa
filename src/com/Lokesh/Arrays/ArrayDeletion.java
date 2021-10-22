13167b126a8325e4c3c751d56b8c50ccad1c5f6e
Lokesh <rocklinglokesh@gmail.com>
2021-10-23 00:09:24 +0530

    initial commit

--- src/com/Lokesh/Arrays/ArrayDeletion.java
+++ src/com/Lokesh/Arrays/ArrayDeletion.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class ArrayDeletion
 {
 	public static void main(String[] args) {
-
-int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-int index = 3;
-System.out.println(remove(array, index));
+	int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	int index = 3;
+	array[index] =  Integer.MIN_VALUE;
+		System.out.println(Arrays.toString(array));
+//	System.out.println(remove(array, index));
 	}
 	static String remove(int[] arr, int index){
 	    for(int i = index; i<=arr.length-1; i++){
@@ -15,9 +16,14 @@ System.out.println(remove(array, index));
 			}
 	        arr[i] = arr[i+1];
 	    }
+
+//	    int[] arr2 = new int[arr.length-2];
+//	    arr2 = arr;
 	    return Arrays.toString(arr);
 	}
 }
+
+
 
 
 
