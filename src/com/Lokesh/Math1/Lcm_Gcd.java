078a1d18187c1478d9bcf9ab65f567c96842132b
Lokesh <rocklinglokesh@gmail.com>
2021-11-03 22:52:21 +0530

    Lcm_Gcd.java

---
+++ src/com/Lokesh/Math1/Lcm_Gcd.java
@@ -0,0 +1,19 @@
+package com.Lokesh.Math1;
+
+public class Lcm_Gcd {
+    public static void main(String[] args) {
+        System.out.println("gcd is: "+ gcd(44,22));
+        System.out.println("LCM is : "+ lcm( 4,8));
+    }
+    static int gcd(int a ,int b)
+    {
+        if(a == 0 ){
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+
+    static  int lcm(int a, int b){
+        return a*b/gcd(a,b);
+    }
+}
