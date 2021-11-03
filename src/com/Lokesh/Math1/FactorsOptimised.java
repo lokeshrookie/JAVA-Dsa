75a44f05124f958d3977397644151c5bc1a0fa6c
Lokesh <rocklinglokesh@gmail.com>
2021-11-03 18:54:22 +0530

    FactorsOptimised

--- src/com/Lokesh/Math1/FactorsOptimised.java
+++ src/com/Lokesh/Math1/FactorsOptimised.java
@@ -2,14 +2,21 @@ package com.Lokesh.Math1;
 
 public class FactorsOptimised {
     public static void main(String[] args) {
-        factors(30);
+        factors(100);
     }
     static  void factors(int n){
         for (int i = 1; i < Math.sqrt(n); i++) {
             if(n % i == 0){
-                System.out.println(i + " " + n/i);
+                System.out.print(i + " " + n/i + " ");
                 //it will print first factor and last factor, 2nd factor and 2nd last factor, ....
+
             }
         }
+        // use arraylist to sort the elements
     }
 }
+
+
+/*
+Output: 1 100 2 50 4 25 5 20
+ */
