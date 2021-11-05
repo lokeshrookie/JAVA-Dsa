d53700b53f6d1fb642662cb52939b8402ed55e41
Lokesh <rocklinglokesh@gmail.com>
2021-11-05 13:51:12 +0530

    NthMagicNumber.java

---
+++ src/com/Lokesh/Math1/BitManipulation/IthBitOfaNumber.java
@@ -0,0 +1,13 @@
+package com.Lokesh.Math1.BitManipulation;
+
+public class IthBitOfaNumber {
+    public static void main(String[] args) {
+//        find(7,1);
+        System.out.println(find(127,7));
+    }
+    static  int find(int n, int k){
+//        return n & (1>>(n-1));
+//        return ((n & (1 << (k-1))) >> (k-1);
+       return   ((n & (1 << (k - 1))) >> (k - 1));
+    }
+}
