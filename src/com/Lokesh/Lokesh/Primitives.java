ac6837980afd2881b06793436a87fbe3189e6321
Lokesh <rocklinglokesh@gmail.com>
2021-11-06 22:06:55 +0530

    initial commit

--- src/com/Lokesh/Lokesh/Primitives.java
+++ src/com/Lokesh/Lokesh/Primitives.java
@@ -8,7 +8,6 @@ public class Primitives { //primitive data type is a data type that we cannot br
     public static void main(String[] args) {
 
         // int rollno = 29_000_000; // Here 29 is literal. and rollno is identifier
-
         // char Section = 'A';
         // float marks = 87.7f;
         // double  largeDecimalNumber = 4567654.4567;
