c2d16c76614166eb0c4b50a36f48d56bc2bf2409
Lokesh <rocklinglokesh@gmail.com>
2021-11-06 22:08:45 +0530

    initial commit

--- src/com/Lokesh/Math1/BitManipulation/NumberOfDigitsInBaseB.java
+++ src/com/Lokesh/Math1/BitManipulation/NumberOfDigitsInBaseB.java
@@ -4,7 +4,7 @@ public class NumberOfDigitsInBaseB {
     public static void main(String[] args) {
         int n = 10;
         int b = 2;
-        int ans = (int) (Math.log(n)/Math.log(b)) + 1;
+        int ans = (int) (Math.log(n)/Math.log(b)) + 1; //
         System.out.println(ans);
     }
 }
