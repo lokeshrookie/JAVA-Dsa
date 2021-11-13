9d5ae7df5b88483e0cecc998dcdaca6d2957aeeb
Lokesh <rocklinglokesh@gmail.com>
2021-11-13 15:43:33 +0530

    LinearSearch using recursion

--- src/com/Lokesh/Recursion/Arrays/LinearSearch.java
+++ src/com/Lokesh/Recursion/Arrays/LinearSearch.java
@@ -3,14 +3,10 @@ package com.Lokesh.Recursion.Arrays;
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
+        int[] array = {11,22,33,44,55};
         System.out.println(findIndex(arr, 3 , 0));
         System.out.println(linear(arr, 3, 0));
-//        if(linear(arr, 4, 0)){
-//            System.out.println(findIndex(arr,3,0));
-//        }
-//        else{
-//            System.out.println("not found");
-//        }
+        System.out.println(findIndexLast(array, 33, arr.length-1)); //checking from last
 
     }
     static  boolean linear(int[] arr , int target , int index){
@@ -29,6 +25,19 @@ public class LinearSearch {
         }
         else{
             return findIndex(arr, target, index+1);
+        }
+    }
+
+    //Search from the last
+    static int findIndexLast(int[] arr, int target , int index){
+        if(index == -1){
+            return  -1;
+        }
+        if(arr[index] == target){
+            return  index;
+        }
+        else{
+            return findIndexLast(arr, target, index-1);
         }
     }
 }
