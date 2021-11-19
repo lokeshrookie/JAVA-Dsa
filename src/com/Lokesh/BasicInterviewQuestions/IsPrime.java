bb3538036a3c8976d330bc5a1ba75214cb27930f
Lokesh <rocklinglokesh@gmail.com>
2021-11-19 13:29:08 +0530

    isprime

--- src/com/Lokesh/BasicInterviewQuestions/IsPrime.java
+++ src/com/Lokesh/BasicInterviewQuestions/IsPrime.java
@@ -7,18 +7,18 @@ public class IsPrime {
     /* note :  we have to use (i*i < = n ) condition
         n < 2 ==> false
      */
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 //        int n = s.nextInt();
 //        System.out.println(isPrime(n));
         for (int i = 0; i < 100; i++) {
             System.out.println(i + " " + isPrime(i));
-
         }
     }
 
-    private static boolean isPrime(int n) {
 
+    private static boolean isPrime(int n) {
         if(n < 2){
             return  false;
         }
@@ -28,6 +28,5 @@ public class IsPrime {
             }
         }
         return  true;
-
     }
 }
