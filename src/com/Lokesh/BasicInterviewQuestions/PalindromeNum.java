204c08a0793613e76138b02359df631231635c71
Lokesh <rocklinglokesh@gmail.com>
2021-11-19 12:33:32 +0530

    PalindromeNum

---
+++ src/com/Lokesh/BasicInterviewQuestions/PalindromeNum.java
@@ -0,0 +1,26 @@
+package com.Lokesh.BasicInterviewQuestions;
+
+import java.util.Scanner;
+
+public class PalindromeNum {
+    public static void main(String[] args) {
+        //    fibonacci series
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(isPalindrome(n));
+    }
+
+
+    private static boolean isPalindrome(int n) {
+        int original = n;
+        int ans = 0;
+        while(n>0){
+            int rem = n%10;
+            ans = ans * 10 + rem;
+            n = n/10;
+
+        }
+
+        return original==ans;
+    }
+}
