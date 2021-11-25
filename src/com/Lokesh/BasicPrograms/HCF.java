4f8524727fb1691a3bf1fa5f099edb21e456d297
Lokesh <rocklinglokesh@gmail.com>
2021-11-25 23:57:10 +0530

    GCD/HCF

---
+++ src/com/Lokesh/BasicPrograms/HCF.java
@@ -0,0 +1,24 @@
+package com.Lokesh.BasicPrograms;
+
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(hcf(a,b));
+//        hcf(a,b);
+    }
+
+    private static int hcf(int a, int b) {
+        int hcf = Math.min(a,b);
+        while (true){
+            if(a%hcf == 0 && b%hcf == 0){
+//                System.out.println("hcf ==  " + hcf);
+                return hcf;
+            }
+            hcf--;
+        }
+    }
+}
