f8adff69aefcff833ef9899a833c4b1bce2f1567
Lokesh <rocklinglokesh@gmail.com>
2021-11-25 23:35:07 +0530

    LCM

--- src/com/Lokesh/BasicPrograms/Lcm.java
+++ src/com/Lokesh/BasicPrograms/Lcm.java
@@ -1,16 +1,13 @@
 package com.Lokesh.BasicPrograms;
-
+import java.util.Scanner;
 /**
  * 1. take two numbers as  input.
  * 2. if any one of them are 0.then lcm will be 0
  * 3. else lcm = max(a,b)
- * 4. Inseide the while loop(TRUE) if(lcm divides both a and b.
+ * 4. Inside the while loop(TRUE) if(lcm divides both a and b.
  *    then return lcm. else check for next number by incrementing the lcm.
  *
  */
-
-import java.util.Scanner;
-
 public class Lcm {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -20,12 +17,10 @@ public class Lcm {
     }
 
     private static int lcm(int a, int b) {
-        int max = Math.max(a,b);
-        int lcm = max;
         if(a == 0 || b == 0){
             return 0;
         }
-
+        int lcm = Math.max(a,b);
         while (true){
             if(lcm%a  == 0 && lcm%b == 0){
                 return lcm;
