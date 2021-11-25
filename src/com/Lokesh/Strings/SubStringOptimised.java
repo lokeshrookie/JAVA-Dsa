7897f93928c975735531e9b9e49c945d36031c96
Lokesh <rocklinglokesh@gmail.com>
2021-11-25 13:50:40 +0530

    Substring

--- src/com/Lokesh/Strings/SubStringOptimised.java
+++ src/com/Lokesh/Strings/SubStringOptimised.java
@@ -3,11 +3,15 @@ package com.Lokesh.Strings;
 public class SubStringOptimised {
     public static void main(String[] args) {
         String s = "loki";
+        sub(s);
+    }
 
-        for (int i = 0; i<s.length(); i++){
-            for(int j = i+1;j<=s.length(); j++){
+    static  void sub(String  s){
+        for(int i = 0; i<s.length(); i++){
+            for(int j = i+1; j<=s.length(); j++){
                 System.out.println(s.substring(i,j));
             }
         }
     }
+
 }
