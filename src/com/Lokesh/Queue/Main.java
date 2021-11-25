e7005c00776704b068c7a5dd892933c5645055ce
Lokesh <rocklinglokesh@gmail.com>
2021-11-25 23:59:27 +0530

    hash

--- src/com/Lokesh/Queue/Main.java
+++ src/com/Lokesh/Queue/Main.java
@@ -1,17 +1,25 @@
 package com.Lokesh.Queue;
 
+import javax.print.attribute.DateTimeSyntax;
+import javax.xml.crypto.Data;
+import java.io.DataInput;
+import java.text.spi.DateFormatProvider;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.Queue;
 
 public class Main {
+
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        queue.add(4);
-        queue.add(5);
-        queue.offer(6);
-        queue.remove();
+
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime ld = LocalDateTime.now();
+        System.out.println(dtf.format(ld));
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Local Date and Time : " + localDateTime);
     }
 }
