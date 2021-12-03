342832696a284fea569045ea731f92fc514b5b1e
Lokesh <rocklinglokesh@gmail.com>
2021-12-03 14:45:33 +0530

    Permutations

---
+++ src/com/Lokesh/Recursion/Permutations/Main.java
@@ -0,0 +1,24 @@
+package com.Lokesh.Recursion.Permutations;
+
+public class Main {
+    public static void main(String[] args) {
+        permutations("", "abc");
+//        System.out.println(permutations("", "abc"););
+    }
+
+    static void permutations(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        for(int i = 0; i<=p.length(); i++){
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            permutations(f + ch + s, up.substring(1));
+        }
+    }
+
+}
+
+
