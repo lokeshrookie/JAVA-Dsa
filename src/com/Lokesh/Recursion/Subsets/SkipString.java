6a50a618d7ab894786272c214ec87cc99db5de3c
Lokesh <rocklinglokesh@gmail.com>
2021-12-04 14:35:06 +0530

    skip string customised

--- src/com/Lokesh/Recursion/Subsets/SkipString.java
+++ src/com/Lokesh/Recursion/Subsets/SkipString.java
@@ -1,21 +1,24 @@
 package com.Lokesh.Recursion.Subsets;
 
+import java.util.Scanner;
+
 public class SkipString {
     public static void main(String[] args) {
 //        String text = "This is apple";
 //        skipString("This is apple");
-        System.out.println(skipString("This is apple"));
+        Scanner s= new Scanner(System.in);
+        String skip = s.nextLine();
+        System.out.println(skipString(skip, "This is apple"));
     }
-
-    static String skipString(String up){
+    static String skipString(String skip, String up){
         if(up.isEmpty()){
             return "";
         }
-        if(up.startsWith("apple")){
-            return skipString(up.substring(5));
+        if(up.startsWith(skip)){
+            return skipString(skip, up.substring(skip.length()));
             //ignoring length of string that need to be skipped.
         }else{
-            return up.charAt(0)+skipString(up.substring(1));
+            return up.charAt(0)+skipString(skip,up.substring(1));
         }
     }
 }
