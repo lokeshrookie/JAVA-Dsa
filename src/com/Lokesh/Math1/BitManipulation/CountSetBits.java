d55ffc92bbeeded933b7cde2a2feed5023cc5380
Lokesh <rocklinglokesh@gmail.com>
2021-12-05 13:37:17 +0530

    CountBits

--- src/com/Lokesh/Math1/BitManipulation/CountSetBits.java
+++ src/com/Lokesh/Math1/BitManipulation/CountSetBits.java
@@ -1,8 +1,10 @@
 package com.Lokesh.Math1.BitManipulation;
 
+import java.util.stream.StreamSupport;
+
 public class CountSetBits {
     public static void main(String[] args) {
-        System.out.println(countSetBits(7));
+        System.out.println(countBits(7));
 
     }
     private static int countSetBits(int n){
@@ -11,6 +13,14 @@ public class CountSetBits {
             count = count + (n&1); // bracket mukyam bigulu.
             // alternative for above line==> count+=n&1;
             n = n>>1; // for removing last digit.
+        }
+        return count;
+    }
+    private static int countBits(int n){
+        int count = 0;
+        while(n > 0){
+            count = count + 1;
+            n>>=1;
         }
         return count;
     }
