8bfaf1070832ca7b73aeefb202cb09ce68a4b803
Lokesh <rocklinglokesh@gmail.com>
2021-12-10 14:58:17 +0530

    Swap explained

--- src/com/Lokesh/BasicInterviewQuestions/Swap.java
+++ src/com/Lokesh/BasicInterviewQuestions/Swap.java
@@ -1,5 +1,14 @@
 package com.Lokesh.BasicInterviewQuestions;
 
 public class Swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println( a + " " + b);
+        a = a + b;  // 11 = 5 + 6;  a = 11, b = 6
+        b = a - b; //  5 = 11 - 6;  a = 11, b = 5
+        a = a - b;//   6 = 11 - 5;  a = 6, b = 5;
+        System.out.println(a + " " + b);
+    }
 
 }
