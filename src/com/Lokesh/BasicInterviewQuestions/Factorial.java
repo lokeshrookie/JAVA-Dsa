61623a5aab1458b65e47b5e8fedc8a7328de3489
Lokesh <rocklinglokesh@gmail.com>
2021-12-10 15:02:55 +0530

    factorial explained

--- src/com/Lokesh/BasicInterviewQuestions/Factorial.java
+++ src/com/Lokesh/BasicInterviewQuestions/Factorial.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner s  = new Scanner(System.in);
-        int n = 5;
-        System.out.println(fact(n));
-
-
+        int n = s.nextInt();
+        System.out.println(factorial(n));
     }
-
+    // iterative way
+    static int factorial (int n){
+        int ans = 1;
+        for(int i = 1; i<=n; i++){
+            ans = ans * i;
+        }
+        return ans;
+    }
+    // recursive way
     private static int fact(int n) {
         if(n < 2 ) {
             return 1;
