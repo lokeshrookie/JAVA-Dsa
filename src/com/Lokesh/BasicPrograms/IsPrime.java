65e182638b6b868da76561e91698a402c8dfc8ac
Lokesh <rocklinglokesh@gmail.com>
2021-12-10 14:39:22 +0530

    isArmstrong improved

--- src/com/Lokesh/BasicPrograms/IsPrime.java
+++ src/com/Lokesh/BasicPrograms/IsPrime.java
@@ -6,9 +6,15 @@ public class IsPrime {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int number = s.nextInt();
-        Boolean b = isPrime(number);
-        System.out.println(b);
+//        for(int i = 1; i<=number; i++){
+//            if(isArmstrong(i)){
+//                System.out.println(i + "yes");
+//            }
+//        }
+//        Boolean b = isPrime(number);
         System.out.println(isArmstrong(number));
+//        System.out.println(b);
+//        System.out.println(isArmstrong(number));
         s.close();
     }
 
@@ -16,14 +22,24 @@ public class IsPrime {
     static boolean isArmstrong(int n) {
         int original = n;
         int sum = 0;
+        int digits = digit(n);
 
-        while (n > 0) {
+        while (n != 0) {
             int rem = n % 10;
             n = n / 10;
-            sum = sum + rem*rem*rem;
+            sum = sum + (int)Math.pow(rem, digits);
         }
 
         return sum == original;
+    }
+
+    private static int digit(int n) {
+        int digits = 0;
+        while(n > 0){
+            n/=10;
+            digits++;
+        }
+        return digits;
     }
 
     static boolean isPrime(int n) {
