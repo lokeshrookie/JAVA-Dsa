65698a39d9914966297e3e5b5e5f9d4a0d9c5c83
Lokesh <rocklinglokesh@gmail.com>
2021-12-11 12:37:09 +0530

    BST implemented with insert, search, traversals

--- src/com/Lokesh/BST.java
+++ src/com/Lokesh/BST.java
@@ -2,6 +2,8 @@ package com.Lokesh;
 
 import com.sun.source.tree.Tree;
 
+import java.util.Scanner;
+
 public class BST {
 
 
@@ -27,6 +29,81 @@ public class BST {
         return root;
     }
 
+    //SEARCH
+    public TreeNode search(TreeNode root, int dataToSearch){
+        // If the root is null, we dont need to check further. so return root.
+        // Also if the data of the root is matched with the dataToSearch, then return root.
+        if(root == null || root.data == dataToSearch){
+            return root;
+        }
+        // if the dataToSearch is smaller than the data of root, then search in the left side of the BST.
+        // this will search for the data recursively.
+        if(root.data > dataToSearch){
+            return search(root.left, dataToSearch);
+        }// if not, search in the right side of the BST.
+        else{
+            return search(root.right, dataToSearch);
+        }
+    }
+
+    //TRAVERSAL METHODS.
+    //INORDER TRAVERSAL: left, root, right
+    public void inOrder(TreeNode root){
+        // if root is null, there is nothing to traverse. so return null
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);  // this will recursively visit all nodes in the left subTree.
+        // after traversing the left subtree, we have to visit root.
+        System.out.print(root.data + " ");
+        // this will recursively visit all node nodes in the right subtree.
+        inOrder(root.right);
+    }
+
+    //PREORDER TRAVERSAL : root, left, right
+    public void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    //POSTORDER TRAVERSAL : left, right, root
+    public void postOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+
+
+
+    //WRAPPER METHODS: ( A wrapper method is an adapter. it provides an alternative interface for an existing method.).
+    //wrapper method are used to make the program simple for the user.
+    // IT ENHANCES THE READABILITY OF THE PROGRAM. IF ANYONE LOOKS AT THE PROGRAM, HE WILL UNDERSTAND THE FLOW OF THE PROGRAM.
+    // LIKE FORM HERE WE ARE GOING TO THIS PARTICULAR METHOD.
+    public void insertIntoTree(int dataToBeInserted){
+        root = insert(root, dataToBeInserted);
+    }
+    public TreeNode searchTree(int dataToBeSearched){
+        return search(root, dataToBeSearched);
+    }
+    public void inOrderTraversal(){
+        inOrder(root);
+    }
+    public  void preOrderTraversal(){
+        preOrder(root);
+    }
+    public void postOrderTraversal(){
+        postOrder(root);
+    }
+
+
 
 
 
@@ -50,4 +127,38 @@ public class BST {
     }
 }
 
+
+class Main{
+    public static void main(String[] args) {
+        BST bst = new BST();
+        int n, num, search;
+        Scanner s = new Scanner(System.in);
+        System.out.println("No of inputs: ");
+        n = s.nextInt();
+        System.out.println("Enter " + n + " numbers");
+
+        for(int i = 0; i<n; i++){
+            num = s.nextInt();
+            bst.insertIntoTree(num);
+        }
+        System.out.println(" Tree insertion is done");
+
+        System.out.println("Enter the nubmer to be searched: ");
+        search = s.nextInt();
+        if(bst.searchTree(search) != null){
+            System.out.println("Number found");
+        }
+        else{
+            System.out.println("Number not found");
+        }
+
+        System.out.println("INORDER TRAVERSAL OF THE BST IS : ");
+        bst.inOrderTraversal();
+        System.out.println("\nPREORDER TRAVERSAL : ");
+        bst.preOrderTraversal();
+        System.out.println("\nPOSTORDER TRAVERSAL : ");
+        bst.postOrderTraversal();
+
+    }
+}
 
