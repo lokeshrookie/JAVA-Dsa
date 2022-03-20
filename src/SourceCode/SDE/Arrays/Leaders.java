e668430d18c2ac02d6bfee28e86c16b519b62b56
Lokesh <rocklinglokesh@gmail.com>
2022-03-20 14:11:24 +0530

    gfg Leaders

--- src/SourceCode/SDE/Arrays/Leaders.java
+++ src/SourceCode/SDE/Arrays/Leaders.java
@@ -1,6 +1,7 @@
 package SourceCode.SDE.Arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Leaders {
@@ -11,8 +12,10 @@ public class Leaders {
         for(int i = 0; i<arr.length; i++){
             arr[i] = s.nextInt();
         }
-        System.out.println(method(arr));
+        System.out.println(leaders(arr));
     }
+
+    /* Method - 1 */
     public static ArrayList<Integer> method(int[] arr){
 //        int[] ans = new int[arr.length];
         ArrayList<Integer> ans = new ArrayList<>();
@@ -22,7 +25,6 @@ public class Leaders {
             }
         }
         ans.add(arr[arr.length-1]);
-
         return ans;
     }
     public static boolean isLeader(int[] arr, int first){
@@ -31,7 +33,36 @@ public class Leaders {
                 return false;
             }
         }
-
         return true;
     }
+
+    /* Method - 2 */
+    public static ArrayList<Integer> leaders(int[] arr){
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = arr.length-1; i>=0; i--){
+            if(isLeader(arr, i)){
+                ans.add(arr[i]);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
