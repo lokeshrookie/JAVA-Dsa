fafbd70f53b47335a2535e9dccbef19d943b678e
Darapureddy Lokesh <rocklinglokesh@gmail.com>
2024-06-25 12:30:14 +0000

    CountPairs

--- src/SourceCode/Arrays/BareMinium/CountPairsOfSum.java
+++ src/SourceCode/Arrays/BareMinium/CountPairsOfSum.java
@@ -5,17 +5,17 @@ class CountPairsWithGivenSum {
         int[] arr1 = {1, 1, 1, 1};
         int sum1 = 2;
         int[] arr2 = {1, 5, 7, 1};
-        int sum2 = 6;
+        int sum2 = 8;
         System.out.println(countPairs(arr2, sum2));
 //        System.out.println(countPairs(arr1,sum1));
         System.out.println(count(arr2,sum2));
     }
     private static int countPairs(int[] arr, int sum) {
         int count = 0;
-        Arrays.sort(arr);
         for(int i = 0; i<arr.length; i++){
             for (int j = i+1; j < arr.length; j++) {
                 if(arr[i] + arr[j] == sum){
+                    System.out.println(arr[i] +  " at " + i + " " +  arr[j] + " at " + j);
                     count++;
                 }
             }
@@ -27,8 +27,8 @@ class CountPairsWithGivenSum {
     static int count(int[] arr , int sum){
         int count = 0;
         for(int i = 0; i<arr.length-1; i++){
-            for(int j = 1; j<arr.length-i; j++){
-                if(arr[j] + arr[j-1] == sum){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == sum){
                     count++;
                 }
             }
